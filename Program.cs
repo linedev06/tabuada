f4da23b39af433df1bf332fef25a94e810675457
linedev06 <vitorafelismino@gmail.com>
2023-06-16 19:39:56 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("--- Tabuada ---");
+
+Console.Write("Digite qualquer número positivo para calcular a tabuada: ");
+int tab = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"A tabuada do valor ({tab}) é:");
+
+for (int i = 1; i <=10; i++ )
+{
+    int resultado = tab * i;
+    Console.WriteLine($"{tab} x {i} = {resultado}");
+
+}
+
+Console.WriteLine("Pressione ENTER para continuar...");
+Console.ReadLine();
+return;
